99029da2dd3de9c50278ed7b1b83ba6f0cc52b20
Kelvin <kelvintenor987@gmail.com>
2024-07-29 20:37:16 -0300

    Initial State from DIO project

---
+++ BancoProjetoDIO/src/Conta.java
@@ -0,0 +1,22 @@
+public class Conta {
+
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public void saque(){}
+
+    public void deposito(){}
+
+    public void tranferencia(){}
+
+    public int getAgencia() {
+        return agencia;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+}
